604c1c7df84b79ac890471389562642b11b03167
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-07-06 07:48:26 +0300

    Update menucrud.php

--- src/routes/backpack/menucrud.php
+++ src/routes/backpack/menucrud.php
@@ -5,15 +5,15 @@
 | Backpack\MenuCRUD Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define or change all of the routes that are
+| This file is where you may define all of the routes that are
 | handled by the Backpack\MenuCRUD package.
 |
 */
 
 Route::group([
-        'prefix' => config('backpack.base.route_prefix'),
-        'middleware' => ['web', 'admin'],
-        'namespace' => 'Backpack\MenuCRUD\app\Http\Controllers\Admin',
-    ], function () {
-        CRUD::resource('menu-item', 'MenuItemCrudController');
-    });
+    'prefix' => config('backpack.base.route_prefix'),
+    'middleware' => ['web', 'admin'],
+    'namespace' => 'Backpack\MenuCRUD\app\Http\Controllers\Admin',
+], function () {
+    CRUD::resource('menu-item', 'MenuItemCrudController');
+});
