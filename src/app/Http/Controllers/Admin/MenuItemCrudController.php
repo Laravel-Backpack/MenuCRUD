d457ee1517807a48feac53a837baec7838afabe9
Antonio Almeida <promatik@gmail.com>
2021-11-28 12:41:34 +0000

    Revert test with fake column

--- src/app/Http/Controllers/Admin/MenuItemCrudController.php
+++ src/app/Http/Controllers/Admin/MenuItemCrudController.php
@@ -53,8 +53,6 @@ class MenuItemCrudController extends CrudController
                 'label' => 'Type',
                 'type' => 'page_or_link',
                 'page_model' => '\Backpack\PageManager\app\Models\Page',
-                'fake' => true,
-                'store_in' => 'extras',
                 'view_namespace' => file_exists(resource_path('views/vendor/backpack/crud/fields/page_or_link.blade.php')) ? null : 'pagemanager::fields',
             ]);
         });
